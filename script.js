8b122e815084d3486879f3c6ba793ebc21470b7a
David Daduut <118278141+Yakdro@users.noreply.github.com>
2025-05-13 21:56:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+//script for scroll function //
+
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+  window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      scrollTopBtn.style.display = "block";
+    } else {
+      scrollTopBtn.style.display = "none";
+    }
+  };
+
+  scrollTopBtn.onclick = function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+
+  // fade function //
+  const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+  threshold: 0.2,
+  rootMargin: "0px 0px -20px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.add("visible");
+    observer.unobserve(entry.target);
+  });
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
+
+window.addEventListener('scroll', () => {
+  scrollTopBtn.style.display = (window.scrollY > 200) ? "block" : "none";
+});
+
